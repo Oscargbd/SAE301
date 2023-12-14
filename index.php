f2750a1b85fa5869f3a0157e17a383f3bcb8a759
Oscargbd <oscarguibaud24@gmail.com>
2023-12-14 16:58:34 +0100

    inclusion de la navbar et du head

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+    include('includes/head.php');
+    ?>
+    <title>Le Trail des Glaces</title>
+</head>
+
 <body>
+    <?php
+    include('includes/navbar.php');
+    ?>
     <h1>YOOOO</h1>
 </body>
+    
+</html>
