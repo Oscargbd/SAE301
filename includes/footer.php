e0b3d6a8757abc4c04a67d4f1a3961948029e3ed
Melenn20 <melenn.riveti@gmail.com>
2023-12-20 14:04:41 +0100

    responsive index.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,22 @@
 <!-- FOOTER -->
-<footer>
-</footer>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../assets/styles.css">
+</head>
+
+<body class="bodyfooter">
+
+    <footer>
+        <img src="../images/flocon2.png" alt="">
+        <h2>Titre</h2>
+        <div id="white-box"></div>
+    </footer>
+</body>
+
+</html>
