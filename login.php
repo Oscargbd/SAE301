9de840c848f42a13da0b19e915cef5a84d7f32e3
Melenn20 <melenn.riveti@gmail.com>
2023-12-22 10:28:05 +0100

    indentation et commentaire
    j'ai commenté le code de certaines pages et je l'ai aussi indenté

--- login.php
+++ login.php
@@ -6,26 +6,36 @@
 <body class="body-login">
     <?php include ("includes/navbar.php") ?>
     <main class='pageLogin'>
+        <!-- Section avec le logo et le titre -->
         <div>
             <img class="img-login" src="img/fondLoginPng.png">
             <h1 class="titrelogin">Le<br>Trail des<br>glaces</h1>
         </div>
+        
+        <!-- Section du formulaire de connexion -->
         <div class="login-form">
             <h2 class="h2-login">Connectez-vous !</h2>
             <form action="actions/loginAction.php" method="post">
+                
                 <label for="email">Email</label>
                 <input class="inputlogin" type="email" id="email" name="email" placeholder="Votre adresse mail" required>
                 <br>
+                
                 <label for="password">Mot de Passe</label>
                 <input class="inputlogin" type="password" id="password" name="password" placeholder="Votre mot de passe" >
                 <br>
+                
                 <button type="submit" >Se connecter</button>
             </form>
-            <p>Vous n'avez pas de compte ? <a href="signup.php">Inscrivez vous</a></p>
+            
+            <!-- Lien vers la page d'inscription -->
+            <p>Vous n'avez pas de compte ? <a href="signup.php">Inscrivez-vous</a></p>
+            
+            <!-- Affichage des messages d'erreur -->
             <div class="errorMSG">
                 <?php
                 if (isset($_SESSION['errorMSG'])) {
-                    ?><p> <?php $_SESSION['errorMSG'] ?></p> <?php ;
+                    ?><p> <?php echo $_SESSION['errorMSG']; ?></p> <?php
                     unset($_SESSION['errorMSG']); // Pour effacer le message après l'avoir affiché
                 }
                 ?>
--- signup.php
+++ signup.php
@@ -5,37 +5,49 @@
 
 <body class="body-login">
     <?php
+    // Inclusion de la barre de navigation
     include('includes/navbar.php');
     ?>
     <main class='pageLogin'>
+        <!-- Section avec le logo et le titre -->
         <div>
             <img class="img-login" src="img/fondLoginPng.png">
             <h1 class="titrelogin">Le<br> Trail des<br> glaces</h1>
         </div>
+        <!-- Section du formulaire d'inscription -->
         <div class="login-form">
             <h2 class="h2-login">Inscrivez vous !</h2>
             <form action="actions/signupaction.php" method="post">
+                
                 <label for="username">Nom d'utilisateur</label>
                 <input class="inputlogin" type="text" id="username" name="username" placeholder="Votre nom d'utilisateur" required>
                 <br>
+                
                 <label for="email">Email</label>
                 <input class="inputlogin" type="email" id="email" name="email" placeholder="Votre adresse mail" required>
                 <br>
+                
                 <label for="password">Mot de Passe</label>
                 <input class="inputlogin" type="password" id="password" name="password" placeholder="Votre mot de passe" required>
                 <br>
+                
                 <label for="nomUtilisateur">Nom</label>
                 <input class="inputlogin" type="text" id="nomUtilisateur" name="nomUtilisateur" placeholder="Votre nom" required>
                 <br>
+                
                 <label for="prenomUtilisateur">Prénom</label>
                 <input class="inputlogin" type="text" id="prenomUtilisateur" name="prenomUtilisateur" placeholder="Votre prénom" required>
                 <br>
+                
                 <label for="ageUtilisateur">Âge</label>
                 <input class="inputlogin" type="text" id="ageUtilisateur" name="ageUtilisateur" placeholder="Votre âge">
                 <br>
+                
                 <button type="submit">S'inscrire</button>
             </form>
+            <!-- Lien vers la page de connexion -->
             <p>Vous avez déjà un compte ? <a href="login.php">Connectez-vous</a></p>
+            <!-- Affichage des messages d'erreur -->
             <div class="errorMSG">
                 <?php
                 if (isset($_SESSION['errorMSG'])) {
